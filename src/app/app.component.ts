4d8db90aec7cf93c4bfabfca9edeaf709df3208b
hamza_ayada <hamza.ayada.ing@gmail.com>
2020-11-03 22:08:50 +0100

    feat: add new component Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Menu} from './menu/menu';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Menue';
+  title = 'Angular-Menu';
+  navItems: Menu[] = [
+    {
+      name : 'link 1',
+      submenus : [ {name : 'submenu 1'}],
+    },
+    {
+      name : 'link 2',
+      submenus : [ {
+        name : 'submenu 2',
+        submenus : [ {
+          name : 'submenu 3'
+        }],
+      }],
+    },
+    {
+      name : 'link 3',
+      submenus : [ {name : 'submenu 4'}],
+    },
+    {
+      name : 'link 4',
+    },
+    {
+      name : 'link 5',
+      submenus : [ {
+        name : 'submenu 5',
+        submenus : [ {
+          name : 'submenu 6'
+        }],
+      }],
+    },
+  ];
+  select(event) {
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {_MatMenuDirectivesModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    _MatMenuDirectivesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
